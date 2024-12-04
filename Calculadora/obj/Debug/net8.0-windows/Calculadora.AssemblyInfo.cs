24d5acc3a015044e32dbb7449b4883263e5ffdcd
Joel Margenet <joel.margenet7@gmail.com>
2024-12-04 03:04:09 +0100

    fixed README

--- Calculadora/obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa47f27a02e1bf52567e179ded201bfc0a8901d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
